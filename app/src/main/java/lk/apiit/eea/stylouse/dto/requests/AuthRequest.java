0d24747cb2be577b68283dfa1d2f00634d9041ca
Fathimath Hanau <hanau.xd@gmail.com>
2020-03-10 22:57:17 +0530

    DTO for Auth request and response.

---
+++ app/src/main/java/lk/apiit/eea/stylouse/dto/requests/AuthRequest.java
@@ -0,0 +1,13 @@
+package lk.apiit.eea.stylouse.dto.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+    private String username;
+    private String password;
+}
