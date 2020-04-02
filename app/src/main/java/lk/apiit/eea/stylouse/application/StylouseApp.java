b3e86dc2c3aaba0fcf2ea4fc52506cf433e483c1
Fathimath Hanau <hanau.xd@gmail.com>
2020-04-03 04:33:31 +0530

    Add application context.

--- app/src/main/java/lk/apiit/eea/stylouse/application/StylouseApp.java
+++ app/src/main/java/lk/apiit/eea/stylouse/application/StylouseApp.java
@@ -3,6 +3,7 @@ package lk.apiit.eea.stylouse.application;
 import android.app.Application;
 
 import lk.apiit.eea.stylouse.di.ApplicationComponent;
+import lk.apiit.eea.stylouse.di.ApplicationModule;
 import lk.apiit.eea.stylouse.di.DaggerApplicationComponent;
 
 public class StylouseApp extends Application{
@@ -11,7 +12,7 @@ public class StylouseApp extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
-        appComponent = DaggerApplicationComponent.create();
+        appComponent = DaggerApplicationComponent.builder().applicationModule(new ApplicationModule((this))).build();
     }
 
     public ApplicationComponent getAppComponent() {
