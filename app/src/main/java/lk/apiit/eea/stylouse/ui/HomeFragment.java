7e65ef3c83f3b929b7fba9d86bb8a45035851ea7
Fathimath Hanau <hanau.xd@gmail.com>
2020-04-03 04:57:41 +0530

    Add SignIn option to the toolbar menu if the user is logged out.

--- app/src/main/java/lk/apiit/eea/stylouse/ui/HomeFragment.java
+++ app/src/main/java/lk/apiit/eea/stylouse/ui/HomeFragment.java
@@ -2,21 +2,52 @@ package lk.apiit.eea.stylouse.ui;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import lk.apiit.eea.stylouse.R;
 import lk.apiit.eea.stylouse.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends HomeBaseFragment {
 
     private FragmentHomeBinding binding;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        if (state == null) {
+            activity.getMenuInflater().inflate(R.menu.profile_items_unauth, menu);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_sign_in: {
+                parentNavController.navigate(R.id.signInFragment);
+                break;
+            }
+            default: {}
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
