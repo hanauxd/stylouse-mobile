64186b37f6fc49f3f6eae513f3f3664c32a72893
Fathimath Hanau <hanau.xd@gmail.com>
2020-04-03 05:00:22 +0530

    Navigate from main fragment to sign out if token is already expired.

--- app/src/main/java/lk/apiit/eea/stylouse/ui/HomeBaseFragment.java
+++ app/src/main/java/lk/apiit/eea/stylouse/ui/HomeBaseFragment.java
@@ -1,49 +1,46 @@
 package lk.apiit.eea.stylouse.ui;
 
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
 import javax.inject.Inject;
 
 import lk.apiit.eea.stylouse.R;
 import lk.apiit.eea.stylouse.application.StylouseApp;
-import lk.apiit.eea.stylouse.di.UserStore;
+import lk.apiit.eea.stylouse.di.AuthSession;
+import lk.apiit.eea.stylouse.models.responses.SignInResponse;
 
-public class HomeBaseFragment extends Fragment {
-
-    protected Activity activity;
-    protected NavController parentNavController;
-
+public class HomeBaseFragment extends RootBaseFragment {
+    @Nullable
     @Inject
-    UserStore userStore;
-
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        this.activity = (Activity) context;
-        parentNavController = Navigation.findNavController(activity.findViewById(R.id.root_nav_host));
-    }
+    SignInResponse state;
+    @Inject
+    AuthSession session;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        StylouseApp applicationInstance = (StylouseApp) activity.getApplicationContext();
-        applicationInstance.getAppComponent().inject(this);
+        ((StylouseApp) activity.getApplication()).getAppComponent().inject(this);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        if(userStore.getUserDetails(activity) == null) {
-            parentNavController.navigate(R.id.signInFragment);
+        if (state != null) {
+            session.setObserver(this, appState -> {
+                if (appState == null) {
+                    parentNavController.navigate(R.id.action_mainFragment_to_signOutFragment);
+                }
+            });
         }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        session.removeObserver(this);
     }
 }
