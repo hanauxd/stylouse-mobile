10ddb377f673f96c5feddbd08e8d044b305e0ce8
Fathimath Hanau <hanau.xd@gmail.com>
2020-04-08 04:51:08 +0530

    Configure dynamic toast warning color

--- app/src/main/java/lk/apiit/eea/stylouse/application/StylouseApp.java
+++ app/src/main/java/lk/apiit/eea/stylouse/application/StylouseApp.java
@@ -2,6 +2,9 @@ package lk.apiit.eea.stylouse.application;
 
 import android.app.Application;
 
+import com.pranavpandey.android.dynamic.toasts.DynamicToast;
+
+import lk.apiit.eea.stylouse.R;
 import lk.apiit.eea.stylouse.di.ApplicationComponent;
 import lk.apiit.eea.stylouse.di.ApplicationModule;
 import lk.apiit.eea.stylouse.di.DaggerApplicationComponent;
@@ -13,6 +16,7 @@ public class StylouseApp extends Application{
     public void onCreate() {
         super.onCreate();
         appComponent = DaggerApplicationComponent.builder().applicationModule(new ApplicationModule((this))).build();
+        DynamicToast.Config.getInstance().setWarningBackgroundColor(getResources().getColor(R.color.colorWarning, null)).apply();
     }
 
     public ApplicationComponent getAppComponent() {
