c6c335f45bcfd758bce7b15bb0b18da6ddb3c662
Fathimath Hanau <hanau.xd@gmail.com>
2020-04-08 05:01:17 +0530

    Add custom constructor and add userId field

--- app/src/main/java/lk/apiit/eea/stylouse/models/requests/SignUpRequest.java
+++ app/src/main/java/lk/apiit/eea/stylouse/models/requests/SignUpRequest.java
@@ -8,10 +8,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SignUpRequest {
+    private String id;
     private String role;
     private String firstName;
     private String lastName;
     private String phone;
     private String email;
     private String password;
+
+    public SignUpRequest(String role, String firstName, String lastName, String phone, String email, String password) {
+        this.role = role;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+    }
 }
