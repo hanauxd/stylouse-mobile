67b527e7a10bb58edc2caa494b7564fae2e7fa4f
Fathimath Hanau <hanau.xd@gmail.com>
2020-04-10 04:35:08 +0530

    Order API  get all orders

---
+++ app/src/main/java/lk/apiit/eea/stylouse/apis/OrderAPI.java
@@ -0,0 +1,13 @@
+package lk.apiit.eea.stylouse.apis;
+
+import java.util.List;
+
+import lk.apiit.eea.stylouse.models.responses.OrdersResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+
+public interface OrderAPI {
+    @GET("orders")
+    Call<List<OrdersResponse>> getOrders(@Header("Authorization") String token);
+}
