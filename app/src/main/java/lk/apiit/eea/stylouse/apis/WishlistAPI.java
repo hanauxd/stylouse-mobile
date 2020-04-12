12bb15b49f9055aea6bb654bd074f25f017078de
Fathimath Hanau <hanau.xd@gmail.com>
2020-04-12 06:54:38 +0530

    Wishlist API interface

---
+++ app/src/main/java/lk/apiit/eea/stylouse/apis/WishlistAPI.java
@@ -0,0 +1,24 @@
+package lk.apiit.eea.stylouse.apis;
+
+import java.util.HashMap;
+import java.util.List;
+
+import lk.apiit.eea.stylouse.models.responses.WishlistResponse;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface WishlistAPI {
+    @POST("wishlist")
+    Call<List<WishlistResponse>> addWishlist(@Header("Authorization") String token, @Body HashMap<String, String> wishlistRequest);
+
+    @DELETE("wishlist/{id}")
+    Call<List<WishlistResponse>> deleteWishlist(@Header("Authorization") String token, @Path("id") String id);
+
+    @GET("wishlist")
+    Call<List<WishlistResponse>> getWishlist(@Header("Authorization") String token);
+}
