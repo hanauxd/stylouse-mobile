0563029b207702ab959e2ff4f46569dfaccc9391
Fathimath Hanau <hanau.xd@gmail.com>
2020-04-12 07:26:09 +0530

    Handle add/delete wishlist, handle unAuth state and render the layout depending on auth state. Handle signIn navigation.

--- app/src/main/java/lk/apiit/eea/stylouse/ui/ProductFragment.java
+++ app/src/main/java/lk/apiit/eea/stylouse/ui/ProductFragment.java
@@ -1,5 +1,6 @@
 package lk.apiit.eea.stylouse.ui;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -16,6 +17,8 @@ import com.google.gson.Gson;
 import com.pranavpandey.android.dynamic.toasts.DynamicToast;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -27,18 +30,87 @@ import lk.apiit.eea.stylouse.databinding.FragmentProductBinding;
 import lk.apiit.eea.stylouse.di.AuthSession;
 import lk.apiit.eea.stylouse.models.requests.CartRequest;
 import lk.apiit.eea.stylouse.models.responses.ProductResponse;
+import lk.apiit.eea.stylouse.models.responses.WishlistResponse;
 import lk.apiit.eea.stylouse.services.CartService;
+import lk.apiit.eea.stylouse.services.WishlistService;
 import retrofit2.Response;
+import timber.log.Timber;
 
 public class ProductFragment extends RootBaseFragment {
     private FragmentProductBinding binding;
     private RecyclerView productSizeList;
     private SizeAdapter adapter;
+    private List<WishlistResponse> wishlists;
 
     @Inject
     AuthSession session;
     @Inject
     CartService cartService;
+    @Inject
+    WishlistService wishlistService;
+
+    private ApiResponseCallback addWishlistCallback = new ApiResponseCallback() {
+        @Override
+        public void onSuccess(Response<?> response) {
+            wishlists = (List<WishlistResponse>) response.body();
+            binding.btnWishlist.setImageResource(R.drawable.icon_favorite);
+            binding.btnWishlist.setColorFilter(Color.RED);
+            DynamicToast.makeSuccess(activity, "Product added to wishlist.").show();
+        }
+
+        @Override
+        public void onFailure(String message) {
+            DynamicToast.makeError(activity, message).show();
+        }
+    };
+
+    private ApiResponseCallback deleteWishlistCallback = new ApiResponseCallback() {
+        @Override
+        public void onSuccess(Response<?> response) {
+            wishlists = (List<WishlistResponse>)response.body();
+            binding.btnWishlist.setImageResource(R.drawable.icon_wishlist);
+            binding.btnWishlist.setColorFilter(Color.RED);
+            DynamicToast.makeSuccess(activity, "Product removed from wishlist.").show();
+        }
+
+        @Override
+        public void onFailure(String message) {
+            DynamicToast.makeError(activity, message).show();
+        }
+    };
+
+    private  ApiResponseCallback isWishlistCallback = new ApiResponseCallback() {
+        @Override
+        public void onSuccess(Response<?> response) {
+            wishlists = (List<WishlistResponse>) response.body();
+            for (WishlistResponse wishlist : wishlists) {
+                if (wishlist.getProduct().getId().equals(binding.getProduct().getId())) {
+                    binding.btnWishlist.setImageResource(R.drawable.icon_favorite);
+                    binding.btnWishlist.setColorFilter(Color.RED);
+                }
+            }
+        }
+
+        @Override
+        public void onFailure(String message) {
+            Timber.e(message);
+        }
+    };
+
+    private ApiResponseCallback addCartCallback = new ApiResponseCallback() {
+        @Override
+        public void onSuccess(Response<?> response) {
+            binding.btnCart.revertAnimation();
+            DynamicToast.makeSuccess(activity, "Product added to cart.").show();
+            parentNavController.navigate(R.id.action_productFragment_to_mainFragment);
+        }
+
+        @Override
+        public void onFailure(String message) {
+            binding.btnCart.revertAnimation();
+            DynamicToast.makeError(activity, message).show();
+        }
+    };
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -56,27 +128,61 @@ public class ProductFragment extends RootBaseFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        bindButtonsToClickListener();
         this.productSizeList = binding.sizeList;
-        binding.btnWishlist.setOnClickListener(this::onAddToWishlistClick);
-        binding.btnCart.setOnClickListener(this::onAddToCartClick);
         initRecyclerView();
         bindProductToView();
     }
 
-    private ApiResponseCallback addCartCallback = new ApiResponseCallback() {
-        @Override
-        public void onSuccess(Response<?> response) {
-            binding.btnCart.revertAnimation();
-            DynamicToast.makeSuccess(activity, "Product added to cart.").show();
-            parentNavController.navigate(R.id.action_productFragment_to_mainFragment);
+    private void bindButtonsToClickListener() {
+        if (session.getAuthState() == null) {
+            binding.btnWishlist.setVisibility(View.GONE);
+            binding.btnCart.setVisibility(View.GONE);
+            binding.btnSignIn.setOnClickListener(this::onSignInClick);
+        } else {
+            binding.btnSignIn.setVisibility(View.GONE);
+            binding.btnWishlist.setOnClickListener(this::onAddToWishlistClick);
+            binding.btnCart.setOnClickListener(this::onAddToCartClick);
+            binding.btnWishlist.setColorFilter(Color.RED);
+        }
+    }
+
+    private void onSignInClick(View view) {
+        parentNavController.navigate(R.id.action_productFragment_to_signInFragment);
+    }
+
+    private void onAddToWishlistClick(View view) {
+        for (WishlistResponse wishlist : wishlists) {
+            if (wishlist.getProduct().getId().equals(binding.getProduct().getId())) {
+                wishlistService.deleteWishlist(wishlist.getId(), deleteWishlistCallback, session.getAuthState().getJwt());
+                return;
+            }
         }
 
-        @Override
-        public void onFailure(String message) {
-            binding.btnCart.revertAnimation();
-            DynamicToast.makeError(activity, message).show();
+        HashMap<String, String> wishlistRequest = new HashMap<>();
+        wishlistRequest.put("productId", binding.getProduct().getId());
+        wishlistService.addWishlist(
+                wishlistRequest,
+                addWishlistCallback,
+                session.getAuthState().getJwt()
+        );
+    }
+
+    private void onAddToCartClick(View view) {
+        int quantity = binding.numberPicker.getProgress();
+        String productId = binding.getProduct().getId();
+
+        if (TextUtils.isEmpty(productId) || TextUtils.isEmpty(adapter.getSize()) || quantity == 0) {
+            DynamicToast.makeWarning(activity, "Select size and quantity").show();
+        } else {
+            binding.btnCart.startAnimation();
+            cartService.addCart(
+                    new CartRequest(productId, adapter.getSize(), quantity),
+                    addCartCallback,
+                    session.getAuthState().getJwt()
+            );
         }
-    };
+    }
 
     private void initRecyclerView() {
         GridLayoutManager layoutManager = new GridLayoutManager(activity, 4);
@@ -86,6 +192,10 @@ public class ProductFragment extends RootBaseFragment {
     }
 
     private void bindProductToView() {
+        if (session.getAuthState() != null) {
+            isProductWishlisted();
+        }
+
         String productJSON = getArguments() != null ? getArguments().getString("product") : null;
         ProductResponse product = new Gson().fromJson(productJSON, ProductResponse.class);
         binding.setProduct(product);
@@ -98,23 +208,10 @@ public class ProductFragment extends RootBaseFragment {
                 .into(binding.productImage);
     }
 
-    private void onAddToCartClick(View view) {
-        if (session.getAuthState() != null) {
-            int quantity = binding.numberPicker.getProgress();
-            String productId = binding.getProduct().getId();
-
-            if (TextUtils.isEmpty(productId) || TextUtils.isEmpty(adapter.getSize()) || quantity == 0) {
-                DynamicToast.makeWarning(activity, "Select size and quantity").show();
-            } else {
-                binding.btnCart.startAnimation();
-                cartService.addCart(
-                        new CartRequest(productId, adapter.getSize(), quantity),
-                        addCartCallback,
-                        session.getAuthState().getJwt()
-                );
-            }
-        } else {
-            DynamicToast.makeError(activity, "Sign in to continue.").show();
-        }
+    private void isProductWishlisted() {
+        wishlistService.getWishlist(
+                isWishlistCallback,
+                session.getAuthState().getJwt()
+        );
     }
 }
