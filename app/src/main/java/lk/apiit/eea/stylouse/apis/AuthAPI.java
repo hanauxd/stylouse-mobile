016bda62cb332b4c7dbaa3fd4bba24a3d36b1c3e
Fathimath Hanau <hanau.xd@gmail.com>
2020-05-21 23:17:07 +0530

    Endpoint for reset-password-request, reset-password-confirmation, and reset-password

--- app/src/main/java/lk/apiit/eea/stylouse/apis/AuthAPI.java
+++ app/src/main/java/lk/apiit/eea/stylouse/apis/AuthAPI.java
@@ -1,11 +1,16 @@
 package lk.apiit.eea.stylouse.apis;
 
+import java.util.Map;
+
 import lk.apiit.eea.stylouse.models.requests.SignInRequest;
 import lk.apiit.eea.stylouse.models.requests.SignUpRequest;
 import lk.apiit.eea.stylouse.models.responses.SignInResponse;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 public interface AuthAPI {
@@ -14,4 +19,14 @@ public interface AuthAPI {
 
     @POST("register")
     Call<ResponseBody> register(@Body SignUpRequest signUpRequest);
+
+    @FormUrlEncoded
+    @POST("/reset-password-request")
+    Call<Map<String, String>> resetPasswordRequest(@Field("email") String email);
+
+    @POST("/reset-password-confirmation")
+    Call<SignInResponse> resetPasswordConfirmation(@Body SignInRequest confirmationRequest);
+
+    @POST("/reset-password")
+    Call<SignInResponse> resetPassword(@Header("Authorization") String token, @Body SignInRequest resetPasswordRequest);
 }
