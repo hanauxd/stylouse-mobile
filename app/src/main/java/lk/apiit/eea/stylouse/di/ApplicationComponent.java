e445399bf08583e8ff2b49d703b664020b1f4e8f
Fathimath Hanau <hanau.xd@gmail.com>
2020-05-21 23:15:33 +0530

    Inject ForgotPasswordFragment, OtpFragment and ResetPasswordFragment

--- app/src/main/java/lk/apiit/eea/stylouse/di/ApplicationComponent.java
+++ app/src/main/java/lk/apiit/eea/stylouse/di/ApplicationComponent.java
@@ -7,11 +7,14 @@ import lk.apiit.eea.stylouse.MainActivity;
 import lk.apiit.eea.stylouse.adapters.CartAdapter;
 import lk.apiit.eea.stylouse.adapters.ProductAdapter;
 import lk.apiit.eea.stylouse.ui.CartFragment;
+import lk.apiit.eea.stylouse.ui.ForgotPasswordFragment;
 import lk.apiit.eea.stylouse.ui.HomeFragment;
 import lk.apiit.eea.stylouse.ui.OrderDetailFragment;
 import lk.apiit.eea.stylouse.ui.OrdersFragment;
+import lk.apiit.eea.stylouse.ui.OtpFragment;
 import lk.apiit.eea.stylouse.ui.ProductFragment;
 import lk.apiit.eea.stylouse.ui.ProfileFragment;
+import lk.apiit.eea.stylouse.ui.ResetPasswordFragment;
 import lk.apiit.eea.stylouse.ui.ShippingFragment;
 import lk.apiit.eea.stylouse.ui.SignInFragment;
 import lk.apiit.eea.stylouse.ui.SignUpFragment;
@@ -21,7 +24,9 @@ import lk.apiit.eea.stylouse.ui.WishlistFragment;
 @Singleton
 @Component(modules = ApplicationModule.class)
 public interface ApplicationComponent {
+
     void inject(MainActivity activity);
+
     void inject(ProfileFragment fragment);
     void inject(SignInFragment fragment);
     void inject(SignUpFragment fragment);
@@ -33,6 +38,10 @@ public interface ApplicationComponent {
     void inject(ProductFragment fragment);
     void inject(ShippingFragment fragment);
     void inject(OrderDetailFragment fragment);
+    void inject(ForgotPasswordFragment fragment);
+    void inject(OtpFragment fragment);
+    void inject(ResetPasswordFragment fragment);
+
     void inject(ProductAdapter adapter);
     void inject(CartAdapter adapter);
 }
