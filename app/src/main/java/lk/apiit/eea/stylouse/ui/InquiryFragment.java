f0db63a0c34390d3a8ad79d0df826023adfe0049
Fathimath Hanau <hanau.xd@gmail.com>
2020-05-25 05:48:24 +0530

    (fix) update recyclerview when new message is sent

--- app/src/main/java/lk/apiit/eea/stylouse/ui/InquiryFragment.java
+++ app/src/main/java/lk/apiit/eea/stylouse/ui/InquiryFragment.java
@@ -1,10 +1,12 @@
 package lk.apiit.eea.stylouse.ui;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,24 +15,28 @@ import androidx.lifecycle.MutableLiveData;
 import com.google.gson.Gson;
 import com.pranavpandey.android.dynamic.toasts.DynamicToast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 
-import lk.apiit.eea.stylouse.R;
+import lk.apiit.eea.stylouse.adapters.InquiryAdapter;
 import lk.apiit.eea.stylouse.apis.ApiResponseCallback;
 import lk.apiit.eea.stylouse.application.StylouseApp;
 import lk.apiit.eea.stylouse.databinding.FragmentInquiryBinding;
 import lk.apiit.eea.stylouse.di.AuthSession;
 import lk.apiit.eea.stylouse.models.Inquiry;
+import lk.apiit.eea.stylouse.models.Reply;
 import lk.apiit.eea.stylouse.models.requests.InquiryRequest;
+import lk.apiit.eea.stylouse.models.responses.InquiryResponse;
 import lk.apiit.eea.stylouse.models.responses.ProductResponse;
 import lk.apiit.eea.stylouse.services.InquiryService;
-import lk.apiit.eea.stylouse.utils.Navigator;
 import retrofit2.Response;
 
 public class InquiryFragment extends RootBaseFragment {
-    private MutableLiveData<String> error = new MutableLiveData<>(null);
+    private MutableLiveData<InquiryResponse> inquiryData = new MutableLiveData<>(null);
     private FragmentInquiryBinding binding;
-    private Inquiry inquiry;
+    private InquiryResponse inquiryResponse;
 
     @Inject
     AuthSession session;
@@ -53,22 +59,29 @@ public class InquiryFragment extends RootBaseFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        error.observe(getViewLifecycleOwner(), this::onErrorChange);
+        inquiryData.observe(getViewLifecycleOwner(), this::onInquiryChange);
         binding.btnSend.setOnClickListener(this::onSendClick);
-        fetchInquiryByUserAndProduct();
+        binding.btnSend.setColorFilter(Color.WHITE);
+        fetchInquiryByProduct();
     }
 
-    private void onErrorChange(String error) {
-        binding.setError(error);
+    private void onInquiryChange(InquiryResponse response) {
+        if (response != null) {
+            List<Inquiry> inquiries = response.getInquiries();
+            List<Reply> replies = new ArrayList<>();
+            inquiries.forEach(inquiry -> {
+                replies.addAll(inquiry.getReplies());
+            });
+            InquiryAdapter adapter = new InquiryAdapter(replies);
+            binding.inquiryList.setAdapter(adapter);
+        }
     }
 
     private void onSendClick(View view) {
-        if (error.getValue() != null) error.setValue(null);
         String inquiryText = binding.inquiry.getText().toString();
         if (TextUtils.isEmpty(inquiryText)) {
-            error.setValue("Inquiry is required");
+            Toast.makeText(activity, "Message is required", Toast.LENGTH_SHORT).show();
         } else {
-            binding.btnSend.startAnimation();
             InquiryRequest inquiry = new InquiryRequest(product().getId(), inquiryText);
             inquiryService.createInquiry(createCallback, session.getAuthState().getJwt(), inquiry);
         }
@@ -79,35 +92,34 @@ public class InquiryFragment extends RootBaseFragment {
         return new Gson().fromJson(productJSON, ProductResponse.class);
     }
 
-    private void fetchInquiryByUserAndProduct() {
-        inquiryService.getInquiryByUserAndProduct(inquiryCallback, session.getAuthState().getJwt(), product().getId());
+    private void fetchInquiryByProduct() {
+        inquiryService.getInquiryByProduct(inquiryCallback, session.getAuthState().getJwt(), product().getId());
     }
 
     private ApiResponseCallback createCallback = new ApiResponseCallback() {
         @Override
         public void onSuccess(Response<?> response) {
-            DynamicToast.makeSuccess(activity, "Inquiry submitted").show();
-            Bundle bundle = new Bundle();
-            bundle.putString("product", new Gson().toJson(product()));
-            Navigator.navigate(parentNavController, R.id.action_inquiryFragment_to_productFragment, bundle);
+            inquiryResponse = (InquiryResponse) response.body();
+            inquiryData.setValue(inquiryResponse);
+            binding.inquiry.setText("");
         }
 
         @Override
         public void onFailure(String message) {
-            binding.btnSend.revertAnimation();
-            error.setValue(message);
+            DynamicToast.makeError(activity, message).show();
         }
     };
 
     private ApiResponseCallback inquiryCallback = new ApiResponseCallback() {
         @Override
         public void onSuccess(Response<?> response) {
-            inquiry = (Inquiry) response.body();
+            inquiryResponse = (InquiryResponse) response.body();
+            inquiryData.setValue(inquiryResponse);
         }
 
         @Override
         public void onFailure(String message) {
-            error.setValue(message);
+            DynamicToast.makeError(activity, message).show();
         }
     };
 }
