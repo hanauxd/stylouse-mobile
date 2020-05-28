ab30a26498343e9e041de8050d4cccda758ed3ac
Fathimath Hanau <hanau.xd@gmail.com>
2020-05-29 02:04:09 +0530

    Initial commit

---
+++ app/src/main/java/lk/apiit/eea/stylouse/ui/InquiriesFragment.java
@@ -0,0 +1,34 @@
+package lk.apiit.eea.stylouse.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import lk.apiit.eea.stylouse.databinding.FragmentInquiriesBinding;
+
+public class InquiriesFragment extends RootBaseFragment {
+    private FragmentInquiriesBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+      binding = FragmentInquiriesBinding.inflate(inflater, container, false);
+      return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
