22eda67d9428c1ccf69ecb0534a61410a63e3d66
Fathimath Hanau <hanau.xd@gmail.com>
2020-05-29 02:02:34 +0530

    Admin fragment initial commit

---
+++ app/src/main/java/lk/apiit/eea/stylouse/ui/AdminFragment.java
@@ -0,0 +1,51 @@
+package lk.apiit.eea.stylouse.ui;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import lk.apiit.eea.stylouse.R;
+import lk.apiit.eea.stylouse.databinding.FragmentAdminBinding;
+
+public class AdminFragment extends RootBaseFragment {
+    private FragmentAdminBinding binding;
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        activity = (Activity) context;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentAdminBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        bottomNavigationView = binding.adminNavigation;
+        NavController navController = Navigation.findNavController(activity, R.id.admin_nav_host);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        bottomNavigationView.setSelectedItemId(R.id.admin_navigation_home);
+    }
+}
