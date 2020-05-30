4658e4791661cab7c7f9bd51e171316391fe5881
Fathimath Hanau <hanau.xd@gmail.com>
2020-05-30 08:05:29 +0530

    Product serice method to update product details

--- app/src/main/java/lk/apiit/eea/stylouse/services/ProductService.java
+++ app/src/main/java/lk/apiit/eea/stylouse/services/ProductService.java
@@ -8,6 +8,7 @@ import lk.apiit.eea.stylouse.apis.ApiResponseCallback;
 import lk.apiit.eea.stylouse.apis.ProductAPI;
 import lk.apiit.eea.stylouse.apis.RetroFitCallback;
 import lk.apiit.eea.stylouse.models.responses.ProductResponse;
+import lk.apiit.eea.stylouse.utils.StringFormatter;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 
@@ -27,5 +28,10 @@ public class ProductService {
     public void getProduct(String id, ApiResponseCallback callback) {
         Call<ProductResponse> productCall = productAPI.getProduct(id);
         productCall.enqueue(new RetroFitCallback<>(callback));
+    }
+
+    public void updateProduct(ApiResponseCallback callback, String jwt, ProductResponse product) {
+        Call<ProductResponse> updateCall = productAPI.updateProduct(StringFormatter.formatToken(jwt), product.getId(), product);
+        updateCall.enqueue(new RetroFitCallback<>(callback));
     }
 }
