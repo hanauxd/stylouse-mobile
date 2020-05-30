7ace5dc4a8ca5726c2cdbe090cfefe0e111216ec
Fathimath Hanau <hanau.xd@gmail.com>
2020-05-30 08:03:51 +0530

    Endpoint to update product details

--- app/src/main/java/lk/apiit/eea/stylouse/apis/ProductAPI.java
+++ app/src/main/java/lk/apiit/eea/stylouse/apis/ProductAPI.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import lk.apiit.eea.stylouse.models.responses.ProductResponse;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface ProductAPI {
@@ -13,4 +16,7 @@ public interface ProductAPI {
 
     @GET("products/{id}")
     Call<ProductResponse> getProduct(@Path("id") String id);
+
+    @POST("products/{id}")
+    Call<ProductResponse> updateProduct(@Header("Authorization") String token, @Path("id") String id, @Body ProductResponse product);
 }
