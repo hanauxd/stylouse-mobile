0bb990d1f13b96a45290113201f7ad9dbfd1b457
Fathimath Hanau <hanau.xd@gmail.com>
2020-06-01 06:39:57 +0530

    Export constants to a dedicated class

---
+++ app/src/main/java/lk/apiit/eea/stylouse/utils/Constants.java
@@ -0,0 +1,9 @@
+package lk.apiit.eea.stylouse.utils;
+
+public class Constants {
+    public static final int PICK_IMAGE_REQUEST = 1;
+
+    public static final String TYPE_UNREAD = "TYPE_UNREAD";
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+    public static final String ROLE_USER = "ROLE_USER";
+}
