834bf024c42a32eb723f513918e2404ec9b3fabe
Fathimath Hanau <hanau.xd@gmail.com>
2020-06-02 02:05:48 +0530

    Import JWT token formatter (static function)

--- app/src/main/java/lk/apiit/eea/stylouse/services/CartService.java
+++ app/src/main/java/lk/apiit/eea/stylouse/services/CartService.java
@@ -11,9 +11,10 @@ import lk.apiit.eea.stylouse.models.requests.CartRequest;
 import lk.apiit.eea.stylouse.models.requests.ShippingRequest;
 import lk.apiit.eea.stylouse.models.responses.CartResponse;
 import lk.apiit.eea.stylouse.models.responses.OrdersResponse;
-import lk.apiit.eea.stylouse.utils.StringFormatter;
 import retrofit2.Call;
 import retrofit2.Retrofit;
+
+import static lk.apiit.eea.stylouse.utils.StringFormatter.formatToken;
 
 public class CartService {
     private CartAPI cartAPI;
@@ -24,22 +25,22 @@ public class CartService {
     }
 
     public void addCart(CartRequest cartRequest, ApiResponseCallback callback, String jwt) {
-        Call<List<CartResponse>> cartCall = cartAPI.addCart(StringFormatter.formatToken(jwt), cartRequest);
+        Call<List<CartResponse>> cartCall = cartAPI.addCart(formatToken(jwt), cartRequest);
         cartCall.enqueue(new RetroFitCallback<>(callback));
     }
 
     public void getCarts(ApiResponseCallback callback, String jwt) {
-        Call<List<CartResponse>> cartsCall = cartAPI.getCarts(StringFormatter.formatToken(jwt));
+        Call<List<CartResponse>> cartsCall = cartAPI.getCarts(formatToken(jwt));
         cartsCall.enqueue(new RetroFitCallback<>(callback));
     }
 
     public void deleteCart(ApiResponseCallback callback, String jwt, String id) {
-        Call<List<CartResponse>> cartsCall = cartAPI.deleteCart(StringFormatter.formatToken(jwt), id);
+        Call<List<CartResponse>> cartsCall = cartAPI.deleteCart(formatToken(jwt), id);
         cartsCall.enqueue(new RetroFitCallback<>(callback));
     }
 
     public void checkout(ApiResponseCallback callback, ShippingRequest shippingRequest, String jwt) {
-        Call<OrdersResponse> checkoutCall = cartAPI.checkout(StringFormatter.formatToken(jwt), shippingRequest);
+        Call<OrdersResponse> checkoutCall = cartAPI.checkout(formatToken(jwt), shippingRequest);
         checkoutCall.enqueue(new RetroFitCallback<>(callback));
     }
 }
--- app/src/main/java/lk/apiit/eea/stylouse/services/ProductService.java
+++ app/src/main/java/lk/apiit/eea/stylouse/services/ProductService.java
@@ -9,10 +9,11 @@ import lk.apiit.eea.stylouse.apis.ProductAPI;
 import lk.apiit.eea.stylouse.apis.RetroFitCallback;
 import lk.apiit.eea.stylouse.models.requests.ProductRequest;
 import lk.apiit.eea.stylouse.models.responses.ProductResponse;
-import lk.apiit.eea.stylouse.utils.StringFormatter;
 import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.Retrofit;
+
+import static lk.apiit.eea.stylouse.utils.StringFormatter.formatToken;
 
 public class ProductService {
     private ProductAPI productAPI;
@@ -27,18 +28,13 @@ public class ProductService {
         productsCall.enqueue(new RetroFitCallback<>(callback));
     }
 
-    public void getProduct(String id, ApiResponseCallback callback) {
-        Call<ProductResponse> productCall = productAPI.getProduct(id);
-        productCall.enqueue(new RetroFitCallback<>(callback));
-    }
-
     public void updateProduct(ApiResponseCallback callback, String jwt, ProductResponse product) {
-        Call<ProductResponse> updateCall = productAPI.updateProduct(StringFormatter.formatToken(jwt), product.getId(), product);
+        Call<ProductResponse> updateCall = productAPI.updateProduct(formatToken(jwt), product.getId(), product);
         updateCall.enqueue(new RetroFitCallback<>(callback));
     }
 
     public void createProduct(ApiResponseCallback callback, String jwt, ProductRequest product, List<MultipartBody.Part> file) {
-        Call<ProductResponse> createCall = productAPI.createProduct(StringFormatter.formatToken(jwt), product, file);
+        Call<ProductResponse> createCall = productAPI.createProduct(formatToken(jwt), product, file);
         createCall.enqueue(new RetroFitCallback<>(callback));
     }
 }
