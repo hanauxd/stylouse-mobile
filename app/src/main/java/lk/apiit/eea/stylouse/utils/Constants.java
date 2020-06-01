1fd70d6012ed48dd0608a3e6780b7e6f526994ea
Fathimath Hanau <hanau.xd@gmail.com>
2020-06-02 01:56:29 +0530

    Add constants TYPE_INBOX, USER_FILE and USER_INFO

--- app/src/main/java/lk/apiit/eea/stylouse/utils/Constants.java
+++ app/src/main/java/lk/apiit/eea/stylouse/utils/Constants.java
@@ -4,6 +4,9 @@ public class Constants {
     public static final int PICK_IMAGE_REQUEST = 1;
 
     public static final String TYPE_UNREAD = "TYPE_UNREAD";
+    public static final String TYPE_INBOX = "TYPE_INBOX";
     public static final String ROLE_ADMIN = "ROLE_ADMIN";
     public static final String ROLE_USER = "ROLE_USER";
+    public static final String USER_FILE = "USER_FILE";
+    public static final String USER_INFO = "USER_INFO";
 }
